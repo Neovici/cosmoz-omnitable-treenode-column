0125843d3241ee70db58c5a7177113b16c8607aa
Meghea Iulian <iulian.meghea@gmail.com>
2020-05-07 18:07:34 +0300

    fix: fixes filter and ui

--- cosmoz-omnitable-treenode-column.js
+++ cosmoz-omnitable-treenode-column.js
@@ -43,7 +43,7 @@ class CosmozOmnitableTreenodeColumn extends columnMixin(PolymerElement) {
 					title="[[ _tooltip ]]"
 					source="[[ _source ]]"
 					text-property="[[ _textProperty ]]"
-					value="[[  _computeValue(filter) ]]"
+					value="[[  _computeValue(filter, _source) ]]"
 					text="[[ query ]]"
 					on-change="[[ _onChange ]]"
 					on-focus="[[ _onFocus ]]"
@@ -153,7 +153,6 @@ class CosmozOmnitableTreenodeColumn extends columnMixin(PolymerElement) {
 			},
 
 			_textProperty: { value: 'text' },
-			_limit: { value: 1 }
 		};
 	}
 
@@ -321,12 +320,12 @@ class CosmozOmnitableTreenodeColumn extends columnMixin(PolymerElement) {
 
 	_computeValue(
 		filter,
-		source = this._source || []
+		source = []
 	) {
 		return source.find(({ value }) => filter === value);
 	}
 	_onChange(value) {
-		this.filter = value?.[0]?.value;
+		this.filter = value?.slice(-1)?.[0]?.value || null;
 	}
 
 	_onFocus(focused) {
