fc192c25eadef42a0d659fe0ca4e4383b187d2f2
Meghea Iulian <iulian.meghea@gmail.com>
2022-11-12 21:42:43 +0200

    fix: use autocomplete
    Replace cosmoz-omnitable-treenode-column-input with simple autocomplete.
    Fixes filter possible infinite loop update.

--- cosmoz-omnitable-treenode-column.js
+++ cosmoz-omnitable-treenode-column.js
@@ -1,30 +1,37 @@
+import { PolymerElement } from '@polymer/polymer/polymer-element';
+import { html, nothing } from 'lit-html';
+import { when } from 'lit-html/directives/when.js';
+import { guard } from 'lit-html/directives/guard.js';
+
 import '@neovici/cosmoz-autocomplete';
 import '@neovici/cosmoz-treenode';
 import '@polymer/paper-spinner/paper-spinner-lite';
 
-import { PolymerElement } from '@polymer/polymer/polymer-element';
-import {
-	html, nothing
-} from 'lit-html';
-
 import { columnMixin } from '@neovici/cosmoz-omnitable/cosmoz-omnitable-column-mixin';
-import './cosmoz-omnitable-treenode-column-input';
 import { get } from '@polymer/polymer/lib/utils/path';
-import { makeCollator } from './utils';
+import { makeCollator, computeTooltip } from './utils';
 import { valuesFrom } from '@neovici/cosmoz-omnitable/lib/utils-data';
 
-const
-	getComparableValue = ({ valuePath, ownerTree, keyProperty, valueProperty }, item) => {
+const getComparableValue = (
+		{ valuePath, ownerTree, keyProperty, valueProperty },
+		item
+	) => {
 		if (!item || !ownerTree) {
 			return;
 		}
 
-		return ownerTree.getPathStringByProperty(get(item, valuePath), keyProperty, valueProperty, ' / ');
+		return ownerTree.getPathStringByProperty(
+			get(item, valuePath),
+			keyProperty,
+			valueProperty,
+			' / '
+		);
 	},
 	getString = (column, item) => getComparableValue(column, item),
-
-	applySingleFilter = ({ valuePath }, filter) => item => filter === get(item, valuePath);
-
+	applySingleFilter =
+		({ valuePath }, filter) =>
+		(item) =>
+			filter === get(item, valuePath);
 
 /**
  * Column that displays a tree node, for `cosmoz-omnitable`.
@@ -41,20 +48,20 @@ class CosmozOmnitableTreenodeColumn extends columnMixin(PolymerElement) {
 			ownerTree: { type: Object, notify: true },
 			locale: { type: String },
 			/**
-			* Name of the property used to lookup the displayed node in the tree
-			*/
+			 * Name of the property used to lookup the displayed node in the tree
+			 */
 			keyProperty: { type: String },
 			valueProperty: { type: String, value: 'name' },
-			minWidth: { type: String, value: '85px' }
+			minWidth: { type: String, value: '85px' },
 		};
 	}
 
 	/**
-	* Get a comparable value from the column.
-	* @param {object} column Column configuration.
-	* @param {object} item Column data.
-	* @returns {void|string} Column data in a comparable format.
-	*/
+	 * Get a comparable value from the column.
+	 * @param {object} column Column configuration.
+	 * @param {object} item Column data.
+	 * @returns {void|string} Column data in a comparable format.
+	 */
 	getComparableValue(column, item) {
 		return getComparableValue(column, item);
 	}
@@ -67,11 +74,11 @@ class CosmozOmnitableTreenodeColumn extends columnMixin(PolymerElement) {
 	}
 
 	/**
-	* Get column represented as a string.
-	* @param {object} column Column configuration.
-	* @param {object} item Item data.
-	* @returns {void|string} Item value in string format.
-	*/
+	 * Get column represented as a string.
+	 * @param {object} column Column configuration.
+	 * @param {object} item Item data.
+	 * @returns {void|string} Item value in string format.
+	 */
 	getString(column, item) {
 		return getString(column, item);
 	}
@@ -103,10 +110,10 @@ class CosmozOmnitableTreenodeColumn extends columnMixin(PolymerElement) {
 				hide-from-root="0"
 				show-max-nodes="0"
 				no-wrap
-				key-property=${ column.keyProperty }
-				.keyValue=${ get(item, column.valuePath) }
-				value-property=${ column.valueProperty }
-				.ownerTree=${ column.ownerTree }
+				key-property=${column.keyProperty}
+				.keyValue=${get(item, column.valuePath)}
+				value-property=${column.valueProperty}
+				.ownerTree=${column.ownerTree}
 			></cosmoz-treenode>
 		`;
 	}
@@ -115,21 +122,48 @@ class CosmozOmnitableTreenodeColumn extends columnMixin(PolymerElement) {
 		return nothing;
 	}
 
-	renderHeader({ title, ownerTree, loading, keyProperty }, { filter }, setState, source) {
-		const spinner = loading
-			? html`<paper-spinner-lite style="width: 20px; height: 20px;" suffix slot="suffix" active></paper-spinner-lite>`
-			: nothing;
+	renderHeader(
+		{ title, ownerTree, loading, keyProperty, valueProperty = 'name' },
+		{ filter },
+		setState,
+		source
+	) {
+		const spinner = when(
+			loading,
+			() => html`<paper-spinner-lite
+				style="width: 20px; height: 20px; flex: none"
+				suffix
+				slot="suffix"
+				active
+			></paper-spinner-lite>`
+		);
 
-		return html`<cosmoz-omnitable-treenode-column-input
-			.title=${ title }
-			.ownerTree=${ ownerTree }
-			.source=${ source }
-			.filter=${ filter }
-			.keyProperty=${ keyProperty }
-			@filter-changed=${ ({ detail: { value }}) => setState(state => ({ ...state, filter: value })) }
-			@header-focused-changed=${ ({ detail: { value }}) => setState(state => ({ ...state, headerFocused: value })) }
-			@query-changed=${ ({ detail: { value }}) => setState(state => ({ ...state, query: value })) }
-		>${ spinner }</cosmoz-omnitable-treenode-column-input>`;
+		return html`<cosmoz-autocomplete
+			.label=${title}
+			.title=${computeTooltip(
+				filter,
+				ownerTree,
+				keyProperty,
+				valueProperty,
+				title
+			)}
+			.source=${source}
+			.textProperty=${'text'}
+			.value=${guard([source, filter], () =>
+				source?.find(({ value }) => filter === value)
+			)}
+			.limit=${1}
+			.onChange=${(value) => {
+				setState((state) => ({
+					...state,
+					filter: value?.slice(-1)?.[0]?.value || null,
+				}));
+			}}
+			.onFocus=${(headerFocused) =>
+				setState((state) => ({ ...state, headerFocused }))}
+			.onText=${(query) => setState((state) => ({ ...state, query }))}
+			>${spinner}</cosmoz-autocomplete
+		>`;
 	}
 
 	/**
@@ -145,20 +179,36 @@ class CosmozOmnitableTreenodeColumn extends columnMixin(PolymerElement) {
 	 *
 	 * @return  {array}                 The sorted values.
 	 */
-	computeSource({ ownerTree, keyProperty, valueProperty, locale = null, valuePath, externalValues, values }, data) {
-		const
-			collator = makeCollator(locale),
-			values_ = externalValues
-				? values
-				: valuesFrom(data, valuePath);
+	computeSource(
+		{
+			ownerTree,
+			keyProperty,
+			valueProperty,
+			locale = null,
+			valuePath,
+			externalValues,
+			values,
+		},
+		data
+	) {
+		const collator = makeCollator(locale),
+			values_ = externalValues ? values : valuesFrom(data, valuePath);
 
 		return values_
-			?.map(value => ({
+			?.map((value) => ({
 				value,
-				text: ownerTree?.getPathStringByProperty(value, keyProperty, valueProperty, ' / ')
+				text: ownerTree?.getPathStringByProperty(
+					value,
+					keyProperty,
+					valueProperty,
+					' / '
+				),
 			}))
 			.sort((a, b) => collator.compare(a.text, b.text));
 	}
 }
 
-customElements.define('cosmoz-omnitable-treenode-column', CosmozOmnitableTreenodeColumn);
+customElements.define(
+	'cosmoz-omnitable-treenode-column',
+	CosmozOmnitableTreenodeColumn
+);
