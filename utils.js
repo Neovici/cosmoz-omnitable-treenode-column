93b1ee815897fc0010776ed09ac9a925a5ff1b40
Joakim Carlsson Kekonius <joakim.carlsson.kekonius@neovici.se>
2023-03-08 12:04:46 +0100

    Adding support for filtering on several nodes (#92)
    * feat(multi value filtering): adding support for filtering on several nodes

--- utils.js
+++ utils.js
@@ -1,30 +1,34 @@
 import { memoize } from '@neovici/cosmoz-utils/memoize';
 
-const
-	/**
-	* Get a language sensitive string comparison object.
-	* @param {string} locale Current locale.
-	* @returns {object} Collator.
-	*/
-	_makeCollator = locale => new Intl.Collator(locale || undefined),
+const /**
+	 * Get a language sensitive string comparison object.
+	 * @param {string} locale Current locale.
+	 * @returns {object} Collator.
+	 */
+	_makeCollator = (locale) => new Intl.Collator(locale || undefined),
 	makeCollator = memoize(_makeCollator),
-
 	/**
-	* Get a tooltip text for the column.
-	* @param {string} filter Filter text.
-	* @param {object} ownerTree Owner tree to get tooltip text from.
-	* @param {string} keyProperty the property to search to find the node
-	* @param {string} valueProperty the value for keyProperty to be the correct node
-	* @param {string} title the title
-	* @returns {string} Tooltip text.
-	*/
-	computeTooltip = (filter, ownerTree, keyProperty, valueProperty, title) => {
-		const requiredValues = [filter, ownerTree, keyProperty, valueProperty];
-		if (requiredValues.includes(null) || requiredValues.includes(undefined)) {
+	 * Get a tooltip text for the column.
+	 * @param {array} filters Filter text.
+	 * @param {string} title the title
+	 * @returns {string} Tooltip text.
+	 */
+	computeTooltip = (filters = [], title) => {
+		if (!Array.isArray(filters) || filters.length < 1) {
 			return title;
 		}
-		return ownerTree.getPathStringByProperty(filter, keyProperty, valueProperty, ' / ');
+		return filters.map((item) => item?.text).join(',');
+	},
+	/**
+	 * Get a filter object for the url.
+	 * @param {array} value Filter text.
+	 * @returns {object} Filter object.
+	 */
+	getCurrentFilter = (value) => {
+		if (value == null || (Array.isArray(value) && value.length === 0)) {
+			return null;
+		}
+		return value;
 	};
 
-
-export { makeCollator, computeTooltip };
+export { makeCollator, computeTooltip, getCurrentFilter };
